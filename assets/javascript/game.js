4aecd5c7f430e1a75e008f61785ebd01f57a788f
RANDOLF RESURRECCION <randolfresurreccion@RANDOLFs-MacBook-Pro.local>
2017-11-30 20:33:02 -0800

    logic works. needs style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,24 @@
-
 var wins = 0;
 var loses = 0;
 var userNum;
 var counter = 0;
 var targetNum;
 var userNum;
-
+var numToGuess;
 
 var reset = function () {
    
-    
     $(".crystals").empty();
     
+    var images = ['https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/c/ce/Crystal_common.png/revision/latest?cb=20151121233809',
+            'https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/c/c8/Crystal_urgentcharacter.png/revision/latest?cb=20151122000356',
+            'https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642',
+            'https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/10/Arena_Crystal.png/revision/latest?cb=20150825214845',]
+  
+   
+    
+     numToGuess = Math.floor(Math.random() * 70) + 30;
+     $("#number-to-guess").html("Number to Guess: " + numToGuess);
     
 
     for (var i = 0; i < 4; i++) {
@@ -19,21 +26,32 @@ var reset = function () {
         console.log(randomNum);
 
         var crystal = $("<div>");
-        crystal.attr({
+            crystal.attr({
             "class": 'crystal',
             "data-num": randomNum
-        });
+            });
+            crystal.css({
+                "background-image":"url('" + (images[i]) + "')",
+                "background-size":"cover",
+                // "background-color":"gray",
+            
+            });
+        
+        
         $(".crystals").append(crystal);
         console.log(crystal);
     }
 }
 
+    
+    
+   
 
 reset();
-var numToGuess = Math.floor(Math.random() * 70) + 30;
-$("#number-to-guess").append(numToGuess);
+// var numToGuess = Math.floor(Math.random() * 70) + 30;
+// $("#number-to-guess").append(numToGuess);
 
-$(".crystal").on("click", function () {
+$(document).on("click", ".crystal", function () {
     var num = parseInt($(this).attr("data-num"));
     counter += num;
     console.log(counter);
@@ -41,17 +59,20 @@ $(".crystal").on("click", function () {
     $("#userNumber").text("Number: " + counter);
 
     if (counter === numToGuess) {
-        wins++
-        $("#wins").append(wins);
+        wins++;
+        alert("Winner Winner.. Chicken Dinner..  " + counter);
+        counter = 0;
+        $("#wins").html("Wins: " + wins);
         reset();
 
     }
     else if (counter > numToGuess) {
-        loses++
-        $("#loses").append(loses);
+        loses++;
+        alert("Final number: " + counter + " Too High!! Try Again");
+        counter = 0;
+        $("#loses").html("Loses: " + loses);
         reset();
     }
 
 
 });
-
